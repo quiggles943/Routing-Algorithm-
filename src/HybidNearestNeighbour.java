18db86550318cab68b8ed98597a0697701458257
quiggles943 <40163180@live.napier.ac.uk>
2016-11-08 15:15:51 +0000

    added swapping and fixed future looking

--- src/HybidNearestNeighbour.java
+++ src/HybidNearestNeighbour.java
@@ -5,72 +5,87 @@ import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class HybidNearestNeighbour {
-
+	static boolean cont = true;
 	public HybidNearestNeighbour() {
 
 	}
 	
 	
-	public static Map<Integer, Point2D> calculate(ArrayList<Point2D> points, int stepsAhead) {
-		Map<Integer,Point2D> result = new HashMap<Integer,Point2D>();
-		ArrayList<Point2D> cities = new ArrayList<Point2D>(points);
-		Point2D currentCity = cities.remove(0);
-		int i = 0;
-		result.put(i,currentCity);
-		while(cities.size() > 0)
-		{
-			//Point2D closest = null;
-			CopyOnWriteArrayList<Point2D> tempcities = new CopyOnWriteArrayList<Point2D>(cities);
-			double  distance = Double.MAX_VALUE;
-			Point2D[] closest = new Point2D[3];
-			
-			for (Point2D city: tempcities)
+	public static Map<Integer, Point2D> calculate(ArrayList<Point2D> points) {
+		Map<Integer,Point2D> shortestresult = new HashMap<Integer,Point2D>();
+		double shortestDistance = Double.MAX_VALUE;
+		int a = 1;
+		do{
+			Map<Integer,Point2D> result = new HashMap<Integer,Point2D>();
+			ArrayList<Point2D> cities = new ArrayList<Point2D>(points);
+			Point2D currentCity = cities.remove(0);
+			int i = 0;
+			result.put(i,currentCity);
+			while(cities.size() > 0)
 			{
-				if(currentCity.distance(city) < distance)
-				{					
-					closest[0] = city;				
-					distance = currentCity.distance(city);
-					tempcities.remove(closest[0]);
-				}
-			}
-			double  distance2 = Double.MAX_VALUE;
-			for (Point2D city: tempcities)
-			{
-				if(currentCity.distance(city) < distance2)
+				CopyOnWriteArrayList<Point2D> tempcities = new CopyOnWriteArrayList<Point2D>(cities);
+				double  distance = Double.MAX_VALUE;
+				Point2D[] closest = new Point2D[3];
+				
+				for (Point2D city: tempcities)
 				{
-					closest[1] = city;				
-					distance2 = currentCity.distance(city);
-					tempcities.remove(closest[1]);
+					if(currentCity.distance(city) < distance)
+					{					
+						closest[0] = city;				
+						distance = currentCity.distance(city);
+						tempcities.remove(closest[0]);
+					}
 				}
-			}
-			double  distance3 = Double.MAX_VALUE;
-			for (Point2D city: tempcities)
-			{
-				if(currentCity.distance(city) < distance3)
+				double  distance2 = Double.MAX_VALUE;
+				for (Point2D city: tempcities)
 				{
-					closest[2] = city;				
-					distance3 = currentCity.distance(city);
-					tempcities.remove(closest[1]);
+					if(currentCity.distance(city) < distance2)
+					{
+						closest[1] = city;				
+						distance2 = currentCity.distance(city);
+						tempcities.remove(closest[1]);
+					}
 				}
-			}
-			double routeDistance = Double.MAX_VALUE;
-			Point2D closestCity = null;
-			for(int j=0;j<=2; j++)
-			{
-				double pathDistance = pathfinding(cities, closest[j], stepsAhead);
-				if(pathDistance <routeDistance)
+				double  distance3 = Double.MAX_VALUE;
+				for (Point2D city: tempcities)
 				{
-					closestCity = closest[j];
-					routeDistance = pathDistance;
+					if(currentCity.distance(city) < distance3)
+					{
+						closest[2] = city;				
+						distance3 = currentCity.distance(city);
+						tempcities.remove(closest[2]);
+					}
 				}
+				double routeDistance = Double.MAX_VALUE;
+				Point2D closestCity = null;
+				for(int j=0;j<=2; j++)
+				{
+					double pathDistance = pathfinding(cities, closest[j], a);
+					if(pathDistance <routeDistance)
+					{
+						closestCity = closest[j];
+						routeDistance = pathDistance;
+					}
+				}
+				cities.remove(closestCity);
+				currentCity = closestCity;
+				result.put(i,closestCity);
+				i++;
 			}
-			cities.remove(closestCity);
-			currentCity = closestCity;
-			result.put(i,closestCity);
-			i++;
-		}
-		result.put(i, result.get(0));
-		return result;
+			result.put(i, result.get(0));
+			if(routeLength(result)<shortestDistance)
+			{
+				shortestDistance = routeLength(result);
+				shortestresult = result;
+			}
+			if(a == result.size())
+			{
+				cont = false;
+			}
+			a++;
+		}while(cont);
+		//shortestresult = swap(shortestresult);
+		return shortestresult;
 	}
 	
 	public static double pathfinding(ArrayList<Point2D> points, Point2D startingPoint, int stepsAhead) {
@@ -123,5 +138,67 @@ public class HybidNearestNeighbour {
 		}
 		return distance;
 	}
+	
+	static Map<Integer, Point2D> swap(Map<Integer, Point2D> route)
+	{
+		Map<Integer,Point2D> temproute = new HashMap<Integer,Point2D>(route);
+		Map<Integer,Point2D> switchingroute = new HashMap<Integer,Point2D>(route);
+		int swaps = switchingroute.size()/4;
+		
+		for (int i = 2;i<swaps-1; i+=2)
+		{
+			Point2D farleft = switchingroute.get(i-1);
+			Point2D left = switchingroute.get(i);
+			Point2D right = switchingroute.get(i+1);
+			Point2D farright = switchingroute.get(i+2);
+			
+			if(farleft.distance(right)<farleft.distance(left))
+			{
+				switchingroute.put(i, right);
+				switchingroute.put(i+1, left);
+			}
+			else{
+				switchingroute.put(i, left);
+				switchingroute.put(i+1, right);
+			}
+			if(farright.distance(farleft)<farright.distance(right))
+			{
+				switchingroute.put(i-1, farright);
+				switchingroute.put(i+2, farleft);
+			}
+			else{
+				switchingroute.put(i-1, farleft);
+				switchingroute.put(i+2, farright);
+				
+			}
+			if(switchingroute.get(i).distance(switchingroute.get(i+2))< switchingroute.get(i).distance(switchingroute.get(i+1)))
+			{
+				Point2D temp = switchingroute.get(i+2);
+				switchingroute.put(i+2, switchingroute.get(i+1));
+				switchingroute.put(i+1, temp);
+				
+			}
+			
+			
+			if(routeLength(switchingroute)<routeLength(temproute))
+			{
+				temproute=switchingroute;
+			}
+			
+			
+		}
+		
+		//if(routeLength(temproute)<routeLength(route))
+		//{
+			return temproute;
+		//}
+		//else return route;
+	}
+	
+	static void stop(){
+		cont = false;
+	}
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,73 @@ import java.util.Map;
 
 
 public class Main {
-	
+	static Thread thread2;
 	static String fileLocation;
 	static ArrayList<Point2D> cities = new ArrayList<Point2D>();
 	static Map<Integer,Point2D> result = new HashMap<Integer,Point2D>();
 	static Map<Integer,Point2D> result2 = new HashMap<Integer,Point2D>();
-	static Map<Integer,Point2D> result3 = new HashMap<Integer,Point2D>();
 	
 	public static void main(String[] args) {
+		basic();
+		Thread thread = new Thread() {
+		    public void run() {
+		    	hybridNearestNeighbour();
+		    }
+		};
+		thread2 = new Thread() {
+		    public void run() {
+		    	boolean notEntered = true;
+		    	do{
+		    	String stopped = null;
+		    	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		    	System.out.println("Enter x to stop at any time");
+		    	try {
+					stopped = reader.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		    	if (stopped.equalsIgnoreCase("x"))
+		    	{
+		    		HybidNearestNeighbour.stop();
+		    		//HybidNearestNeighbour.cont = false;
+		    		notEntered = false;
+		    	}
+		    	}while(notEntered);
+		    	
+		    }
+		};
 		
+		thread.start();
+		
+		
+		
+
+	}
+	
+	public static void hybridNearestNeighbour(){
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter a file to load");
+		try {
+			fileLocation = reader.readLine();
+			cities = LoadCities.loadTSPLib(fileLocation);
+			thread2.start();
+			result = HybidNearestNeighbour.calculate(cities);
+			double routeLength = HybidNearestNeighbour.routeLength(result);
+			System.out.println("The length of the  nearest neighbour algorithm route with pathfinding is "+routeLength);
+			TSPIllustration.IllustrateRoute(result, (float)5, fileLocation+" Route wth pathfinding");
+			//TSPIllustration.IllustrateAll(result, (float) 3, "Route step");
+			result = HybidNearestNeighbour.swap(result);
+			double routeLength2 = HybidNearestNeighbour.routeLength(result);
+			System.out.println("The length of the  nearest neighbour algorithm route with pathfinding and swapping is "+routeLength2);
+			TSPIllustration.IllustrateRoute(result, (float)5, fileLocation+" Route wth pathfinding and swap");
+			}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void proof(){
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Please enter a file to load");
 		try {
@@ -25,9 +83,7 @@ public class Main {
 		cities = LoadCities.loadTSPLib(fileLocation);
 		result = NearestNeighbour.nearestNeighbour(cities);
 		cities = LoadCities.loadTSPLib(fileLocation);
-		result2 = RandomNearestNeighbour.randomNearestNeighbour(cities, 22);
-		cities = LoadCities.loadTSPLib(fileLocation);
-		result3 = HybidNearestNeighbour.calculate(cities, 280);
+		result2 = HybidNearestNeighbour.calculate(cities);
 		
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -35,15 +91,32 @@ public class Main {
 		System.out.println("Calculations complete");
 
 		double routeLength = NearestNeighbour.routeLength(result);
-		double routeLength3 = HybidNearestNeighbour.routeLength(result3);
+		double routeLength3 = HybidNearestNeighbour.routeLength(result2);
 
 		System.out.println("The length of the  nearest neighbour algorithm route is "+routeLength);
 		System.out.println("The length of the  nearest neighbour algorithm route with pathfinding is "+routeLength3);
-		TSPIllustration.IllustrateRoute(result, (float) 20, fileLocation+" Route");
-		TSPIllustration.IllustrateRoute(result3, (float)20, fileLocation+" Route wth pathfinding");
-		//TSPIllustration.IllustrateAll(result, (float) 0.8, fileLocation+" Route");
-		//TSPIllustration.IllustrateAll(result3, (float) 0.8, fileLocation+" Route wth pathfinding");
+		TSPIllustration.IllustrateRoute(result, (float) 2, fileLocation+" Route");
+		TSPIllustration.IllustrateRoute(result2, (float)2, fileLocation+" Route wth pathfinding");
+	}
+	
+	public static void basic()
+	{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter a file to load");
+		try {
+			fileLocation = reader.readLine();
+		
+		cities = LoadCities.loadTSPLib(fileLocation);
+		result = NearestNeighbour.nearestNeighbour(cities);
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Calculations complete");
 
+		double routeLength = NearestNeighbour.routeLength(result);
+		System.out.println("The length of the  nearest neighbour algorithm route is "+routeLength);
+		TSPIllustration.IllustrateRoute(result, (float) 5, fileLocation+" Route");
 	}
 
 }
